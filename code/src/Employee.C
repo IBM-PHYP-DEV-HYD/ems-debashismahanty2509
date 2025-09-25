ae987a0ce11f9bb9d9dfb06778fea72624eecc6a
Debashis <Debashis.mahanty@ibm.com>
2025-09-25 12:27:28 +0530

    Handled the DOL,ID automatically after changing the EmpType

--- code/src/Employee.C
+++ code/src/Employee.C
@@ -2,9 +2,12 @@
 
 size_t Employee::mMemberNumbers = 0;
 
-Employee::Employee() : mEmployeeType(EmpType::NONE), mEmployeeStatus(EmpStatus::ACTIVE), mDOJ(""), mDOL(""), mName(""), mID(""), mGender('N'), mDOB("")
+Employee::Employee() : mEmployeeType(EmpType::NONE), mEmployeeStatus(EmpStatus::ACTIVE), mGender('N'), mDOB("NA")
 {
-
+    std::cout<< "Enter Name: ";
+    std::getline(std::cin>>std::ws,mName);
+    setIDBasedOnMemberNumberAndType(mEmployeeType);
+    setDOJ();
 }
 Employee::Employee(EmpType empTypeParam, EmpStatus empStatusParam) : mEmployeeType(empTypeParam), mEmployeeStatus(empStatusParam)
 {
@@ -25,13 +28,7 @@ Employee::Employee(EmpType empTypeParam, EmpStatus empStatusParam) : mEmployeeTy
     std::cout<<"Enter DOB (DD-MM-YYYY): ";
     std::getline(std::cin>>std::ws,mDOB);
 
-    std::cout<<"Enter DOJ (DD-MM-YYYY): ";
-    std::getline(std::cin>>std::ws,mDOJ);
-
-    setDOLBasedOnDOJ();
-    
-    
-
+    setDOJ();
 }
 
 Employee::~Employee()
@@ -39,31 +36,63 @@ Employee::~Employee()
 
 }
 
-void Employee::setDOLBasedOnDOJ(void)
+void Employee::setDOLBasedOnDOJ(bool isDOJSet, std::string basedOnTodayDate = "")
 {
-    if(mDOJ.length() != 10 || mEmployeeType == EmpType::NONE || mEmployeeType == EmpType::FULLTIME)
+    if(isDOJSet == true)
     {
-        mDOL = "NA";
-        return;
-    }
-    else if(mEmployeeType == EmpType::CONTRACTUAL)
-    {
-        mDOL = mDOJ.substr(0,6) + std::to_string(std::stoi(mDOJ.substr(6,4)) + 1);
-        return;
-    }
-    else if(mEmployeeType == EmpType::INTERN)
-    {
-        int month = std::stoi(mDOJ.substr(3,2));
-        int year = std::stoi(mDOJ.substr(6,4));
-        month += 6;
-        if(month > 12)
+        if(mDOJ.length() != 10 || mEmployeeType == EmpType::NONE || mEmployeeType == EmpType::FULLTIME)
         {
-            month -= 12;
-            year += 1;
+            mDOL = "NA";
+            mDOJ.length() != 10 ? std::cout<<"DOJ is not set properly\n"<<std::endl : std::cout<<"DOL is set to NA\n"<<std::endl;
+            return;
         }
-        std::string monthStr = (month < 10) ? "0" + std::to_string(month) : std::to_string(month);
-        mDOL = mDOJ.substr(0,3) + monthStr + "-" + std::to_string(year);
-        return;
+        else if(mEmployeeType == EmpType::CONTRACTUAL)
+        {
+            mDOL = mDOJ.substr(0,6) + std::to_string(std::stoi(mDOJ.substr(6,4)) + 1);
+            return;
+        }
+        else if(mEmployeeType == EmpType::INTERN)
+        {
+            int month = std::stoi(mDOJ.substr(3,2));
+            int year = std::stoi(mDOJ.substr(6,4));
+            month += 6;
+            if(month > 12)
+            {
+                month -= 12;
+                year += 1;
+            }
+            std::string monthStr = (month < 10) ? "0" + std::to_string(month) : std::to_string(month);
+            mDOL = mDOJ.substr(0,3) + monthStr + "-" + std::to_string(year);
+            return;
+        }
+    }
+    else
+    {
+        if(basedOnTodayDate.length() != 10 || mEmployeeType == EmpType::NONE || mEmployeeType == EmpType::FULLTIME)
+        {
+            mDOL = "NA";
+            basedOnTodayDate.length() != 10 ? std::cout<<"DOJ is not set properly\n"<<std::endl : std::cout<<"DOL is set to NA\n"<<std::endl;
+            return;
+        }
+        else if(mEmployeeType == EmpType::CONTRACTUAL)
+        {
+            mDOL = basedOnTodayDate.substr(0,6) + std::to_string(std::stoi(basedOnTodayDate.substr(6,4)) + 1);
+            return;
+        }
+        else if(mEmployeeType == EmpType::INTERN)
+        {
+            int month = std::stoi(basedOnTodayDate.substr(3,2));
+            int year = std::stoi(basedOnTodayDate.substr(6,4));
+            month += 6;
+            if(month > 12)
+            {
+                month -= 12;
+                year += 1;
+            }
+            std::string monthStr = (month < 10) ? "0" + std::to_string(month) : std::to_string(month);
+            mDOL = basedOnTodayDate.substr(0,3) + monthStr + "-" + std::to_string(year);
+            return;
+        }
     }
 }
 void Employee::setIDBasedOnMemberNumberAndType(EmpType& empTypeParam)
@@ -134,6 +163,10 @@ const std::string_view Employee::getDOB(void) const
 void Employee::setEmployeeType(EmpType empTypeParam) 
 { 
     mEmployeeType = empTypeParam; 
+    changeIdwithEmpType();
+    std::string sDate;
+    getTodayDate(sDate);
+    setDOLBasedOnDOJ(false,sDate);
 }
 
 void Employee::setEmployeeStatus(EmpStatus empStatusParam) 
@@ -144,4 +177,40 @@ void Employee::setEmployeeStatus(EmpStatus empStatusParam)
 void Employee::setDOL(const std::string& dolParam) 
 { 
     mDOL = dolParam; 
+}
+void Employee::setDOJ(void)
+{
+    // take today date as DOJ
+    getTodayDate(mDOJ);
+    setDOLBasedOnDOJ(true);
+}
+
+void Employee::getTodayDate(std::string& dateParam)
+{
+    time_t sNow = time(0);
+    tm *sLtm = localtime(&sNow);
+    dateParam = (sLtm->tm_mday < 10 ? "0" : "") + std::to_string(sLtm->tm_mday) + "-" +
+                (sLtm->tm_mon + 1 < 10 ? "0" : "") + std::to_string(sLtm->tm_mon + 1) + "-" +
+                std::to_string(1900 + sLtm->tm_year);
+}
+
+void Employee::changeIdwithEmpType(void)
+{
+    mID = mID.substr(0, mID.length() - 1); // Remove the last character
+    if(mEmployeeType == EmpType::INTERN)
+    {
+        mID += "I";
+    }
+    else if(mEmployeeType == EmpType::FULLTIME)
+    {
+        mID += "F";
+    }
+    else if(mEmployeeType == EmpType::CONTRACTUAL)
+    {
+        mID += "C";
+    }
+    else
+    {
+        mID += "N";
+    }
 }
