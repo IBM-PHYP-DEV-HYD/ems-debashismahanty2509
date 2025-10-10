386e5c3098babd37e31afd130a25b402d00d39d0
Debashis <Debashis.mahanty@ibm.com>
2025-10-10 18:20:16 +0530

    bug fix in Manager displayemp

--- code/src/EMS.C
+++ code/src/EMS.C
@@ -13,5 +13,12 @@ int main()
 {
     EMS ems;
     ems.managerPtr = Manager<std::string>::getInstance("XYZ Corp", "admin", "password123");
+    if(NULL != ems.managerPtr)
+    {
+        ems.managerPtr->addEmployee(Employee::EmpType::FULLTIME);
+        ems.managerPtr->addEmployee(Employee::EmpType::INTERN);
+        ems.managerPtr->addEmployee(Employee::EmpType::CONTRACTUAL);
+        ems.managerPtr->displayAllEmployee();
+    }
     return 0;
 }
